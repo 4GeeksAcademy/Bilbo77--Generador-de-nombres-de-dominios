45cf6b81f7b311ea90cbb3a48bc21bb64254277e
Bilbo L.R <bilbo.lr@gmail.com>
2024-03-30 13:36:09 +0000

    Nueva Rama

--- src/app.js
+++ src/app.js
@@ -16,58 +16,62 @@ let domain = ['.com','.es','.net'];
 
 // Hacer el recorrido usando For Length
 
-let textForLength = '<ul class="list-group">'
-textForLength += '<li class = "list-group-item bg-success"> Using for length </li>'
-
+let usandoForLength = '<ul class="list-group">'
+usandoForLength += '<li class="list-group-item bg-danger"> Usando For Length </li>'
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < domain.length; l++) {
-        textForLength += '<li class = "list-group-item">' + pronoun[i] + adj[j] + noun[k] + domain[l] + '</li>'
-      }     
-    } 
+        usandoForLength += `<li class="list-group-item">${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}</li>`
+      }      
+    }
   }
 }
-textForLength += '</ul>'
-document.querySelector('#forLength').innerHTML = textForLength;
+usandoForLength += '</ul>'
+document.querySelector('#forLength').innerHTML = usandoForLength
 
 
+// Hacer el recorrido usando For Off
 
-// Hacer el recorrido usando for off
-
-let textForOff = '<ul class="list-group">'
-  textForOff += '<li class = "list-group-item bg-success">Using for off</li>'
-for (const iterator of pronoun) {
-  for (const item of adj) {
-    for (const element of noun) {
-      for (const coeff of domain) {
-        textForOff += `<li class = "list-group-item"> ${iterator}${item}${element}${coeff}</li>`
+let usandoForOff = '<ul class = "list-group">'
+usandoForOff += '<li class="list-group-item bg-danger"> Usando For Off</li>'
+for (const item of pronoun) {
+  for (const element of adj) {
+    for (const iterator of noun) {
+      for (const fact of domain) {
+        usandoForOff += `<li class="list-group-item">${item}${element}${iterator}${fact}</li>`
       }
     }
   }
 }
-  textForOff += '</ul>'
-document.querySelector('#forOff').innerHTML = textForOff
+usandoForOff += '</ul>'
+document.querySelector('#forOff').innerHTML = usandoForOff
+
 
 
 // Haciendo el recorrido con .map()
 
-let textMap = '<ul class="list-group">'
-textMap += '<li class = "list-group-item bg-success"> Using .map()</li>'
-pronoun.map((iterator) => {
-  adj.map((item) => {
-    noun.map((element) => {
-      domain.map((coeff) => {
-        textMap += `<li class = "list-group-item">${iterator}${item}${element}${coeff}</li>`
+let usandoMap = '<ul class="list-group">'
+usandoMap += '<li class="list-group-item bg-danger"> Usando .map</li>'
+pronoun.map((item) => {
+  adj.map((element) => {
+    noun.map((iterator) => {
+      domain.map((fact) => {
+        usandoMap += `<li class="list-group-item">${item}${element}${iterator}${fact}</li>`
       })
     })
   })
 })
-textMap += '</ul>'
-document.querySelector('#map').innerHTML = textMap;
+usandoMap += '</ul>'
+document.querySelector('#map').innerHTML = usandoMap
 
-console.log(textMap);
+
+
 };
+                                                        
+
+
+
 
 // Creamos por ejemplo 4 nuevos arrays con objetos
 
@@ -78,52 +82,52 @@ let reaction = ['¡¿¡ WDF !?!', '¡Qué notas!']
 
 // Usando un .map()
 
-let RastreadorMap = '<ul class="list-group">'
-RastreadorMap += '<li class="list-group-item bg-primary">Usando el método Map</li>'
+let usandoElMetodoMap = '<ul class="list-group" >'
+usandoElMetodoMap += '<li class="list-group-item bg-danger"> Usando el método .map</li>'
 who.map((item) => {
   action.map((element) => {
     object.map((iterator) => {
-      reaction.map((factor) => {
-        RastreadorMap += `<li class="list-group-item">${item} ${element} ${iterator} ,${factor}</li>`
+      reaction.map((fact) => {
+        usandoElMetodoMap += `<li class="list-group-item">${item} ${element} ${iterator}, ${fact}</li>`
       })
     })
   })
 })
-RastreadorMap += '</ul'
-document.querySelector('#métodomap').innerHTML = RastreadorMap;
+usandoElMetodoMap += '</ul>'
+document.querySelector('#métodomap').innerHTML = usandoElMetodoMap
 
 // Usando for length
 
-let RastreadorForLength = '<ul class="list-group">'  
-  RastreadorForLength += '<li class="list-group-item bg-primary">Usando el bucle For Length/loop</li>'
-  for (let i = 0; i < who.length; i++) {
-    for (let j = 0; j < action.length; j++) {
-      for (let k = 0; k < object.length; k++) {
-        for (let l = 0; l < reaction.length; l++) {
-          RastreadorForLength +='<li class="list-group-item">' + who[i] + ' ' + action[j] + ' '  + object[k] + ' ,'  + reaction[l] + '</li>' 
-        }        
-      }      
-    }    
-  }
-  RastreadorForLength += '</ul>'
-document.querySelector('#métodoForLength').innerHTML = RastreadorForLength;
-
-// Usandp el bucle For off
-
-let RastreadorForOff = '<ul class="list-group">'
-RastreadorForOff += '<li class="list-group-item bg-primary">Usando el buclue For Off</li>'
-for (const iterator of who) {
-  for (const item of action) {
-    for (const element of object) {
-      for (const factor of reaction) {
-        RastreadorForOff += `<li class="list-group-item">${iterator} ${item} ${element} ,${factor}</li>`
+let usandoElBucleForLenght = '<ul class="list-group">'
+usandoElBucleForLenght += '<li class="list-group-item bg-danger"> Usando  el bucle For Length</li>'
+for (let a = 0; a < who.length; a++) {
+  for (let b = 0; b < action.length; b++) {
+    for (let c = 0; c < object.length; c++) {
+      for (let d = 0; d < reaction.length; d++) {
+        usandoElBucleForLenght += `<li class="list-group-item">${who[a]} ${action[b]} ${object[c]}, ${reaction[d]}>/li>`
       }
     }
   }
 }
-RastreadorForOff += '</ul>'
-document.querySelector('#métodoForOff').innerHTML = RastreadorForOff
-console.log(RastreadorForLength);
+usandoElBucleForLenght += '</ul>'
+document.querySelector('#métodoForLength').innerHTML = usandoElBucleForLenght
+
+
+// Usand el bucle For off
+
+let usandoElBucleForOff = '<ul class = "list-group">'
+usandoElBucleForOff += '<li class = "list-group-item bg-danger"> Usando el bucle For Off</li>'
+for (const w of who) {
+  for (const x of action) {
+    for (const y of object) {
+      for (const z of reaction) {
+        usandoElBucleForOff += `<li class = "list-group-item">${w} ${x} ${y}, ${z}</li>`
+      }
+    }
+  }
+}
+usandoElBucleForOff += '</ul>'
+document.querySelector('#métodoForOff').innerHTML = usandoElBucleForOff
 
 
 
